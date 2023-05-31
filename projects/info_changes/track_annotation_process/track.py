dcf9235953a799ff2edbcbaaa0321259846c6faa
AmelieW <amelie.wuehrl@gmx.de>
2023-05-31 16:01:05 +0200

    adding script to track annotation process

---
+++ projects/info_changes/track_annotation_process/track.py
@@ -0,0 +1,70 @@
+from utils.utils import Annotator, LabeledInstance, instance_stats, group_instances_wrt_id
+import pandas as pd
+import os
+import argparse
+
+#L = ['5caad602a15c9800161ad6d3', '61671c6dc5d5587bdaf08989', '6303814dfe5bbd0b827d10b4', '5e8783b0fde5153fbd9dca43', '5f0448506587140241493cbe', '60dc54db60a4dca481ba3608', '61001309835f7fddeb965ea9', '60cf529b26650af60dbc0440', '60ccbcfaa216558af6d8bf0e', '60a2d1593f270f92fa69305d']
+#P= "/Users/amelie/Documents/1_Uni/Promotion/admin/research-stay/information-changes/data/anno-environment/potato/projects/info_changes/pilot/batch-1_annotation_output/"
+#5e820a415b6ebc01e6cd81a8/annotated_instances.jsonl"
+
+P= "/Users/amelie/Documents/1_Uni/Promotion/admin/research-stay/information-changes/data/anno-environment/potato/projects/info_changes/pilot/batch-2_annotation_output/"
+L = ['5ce33217f1117f0017c80de1', '60d3867b6e7529fc77032b54', '6168926c2ebd65e2f7eee82c', '5c1bb460a05a64000125c522', '5c131126d6d169000148414a', '60e2ebb6ad1d5ee0e515a7c5', '614dd12e1864621b9d8436ac', '5f96b94504b0720009b4a1ed', '6072e4e6236de7ca7d4bf06e', '61747e90d6980cdffa3de99d', '5fdf4ff8b112b67125e0b2b1', '61572a3b2955ccd1969daecd', '5d6d15ce424136001af41fbc']
+def parse_args():
+    # use the argparse module to parse commandline arguments
+
+    parser = argparse.ArgumentParser(description='Evaluate the annotation process')
+    # add argument for the directory with the annotation output
+    parser.add_argument('-d', '--directory', type=str, required=True,
+                        help='the directory with the annotation output')
+
+    return parser.parse_args()
+
+def run():
+    """starts the eval pipeline"""
+
+    all_annotations = []
+
+    # create a list of all Prolific Ids in the directory:
+    L = os.listdir(args.directory)
+    # make sure the list only contains directories with prolific ids as names:
+    L = [l for l in L if len(l) == 24]
+    
+
+    for prolific_id in L:
+        file = f"{P}{prolific_id}/annotated_instances.jsonl"
+
+        # check if the annotator did anything (this removes people who returned from the study without doing anything)
+        if not os.path.exists(file):
+            continue
+
+        #create Annotator obj.
+        annotator = Annotator(prolific_id)
+
+        # read in their data:
+        df = pd.read_json(file, lines=True)
+        ignore = ["Consent.html", "Experience.html"]
+        for d, id in enumerate(df["id"]):
+            if id not in ignore:
+                instance = LabeledInstance(id)
+                instance.add_labeled_instance(prolific_id,
+                                              df.at[d, "displayed_text"],
+                                              df.at[d, "label_annotations"],
+                                              df.at[d, "span_annotations"],
+                                              df.at[d, "behavioral_data"])
+                annotator.add_new_instance(instance)
+
+        all_annotations.append(annotator)
+
+    instance_stats(all_annotations)
+
+    # for each instance id, get the annotations for this instacne grouped
+    instance_groups = group_instances_wrt_id(all_annotations)
+    for g in instance_groups:
+        print(g)
+
+
+
+if __name__ == "__main__":
+    # parse arguments from the commandline with argparse
+    args = parse_args()
+    run(args)
---
+++ projects/info_changes/track_annotation_process/utils/utils.py
@@ -0,0 +1,152 @@
+
+
+class Annotator:
+    def __init__(self, anno_id):
+        self.id = anno_id
+        self.instances = []
+
+    def add_new_instance(self, labeled_instance):
+        self.instances.append(labeled_instance)
+
+
+class LabeledInstance:
+    def __init__(self, instance_id):
+        self.id = instance_id
+
+    def __str__(self):
+        return f"id: {self.id}\n" \
+               f"instance text: {self.instance_text}\n" \
+               f"labels: {self.labels}\n" \
+               f"spans: {self.spans}\n" \
+               f"meta_data: {self.meta_data}"
+
+    def parse_annotations(self, label_anno, span_anno):
+        """takes in the label_annotations and span_annoations that we get from potato and extract it into attributes"""
+        # {'changes-key-entity': {'no': '2'}, 'Level-of-generality': {'no': '2'}, 'how-certain-is-the-reported-finding': {'Somewhat certain': '4'}, 'how-certain-is-the-paper-finding': {'Certain': '5'}, 'reporting-is-sensational': {'Neutral': '1'}, 'paper-is-sensational': {'Neutral': '1'}, 'how-causal-is-the-reporting': {'Correlation': '2'}, 'how-causal-is-the-paper-finding': {'Correlation': '2'}}
+        labels = []
+        spans = []
+
+        for item in label_anno.items():
+            label_name = item[0]
+            for key in item[1].keys():
+                label_value_name = key
+                label_value_numeric = item[1][key]
+            labels.append((label_name, label_value_name, label_value_numeric))
+        if span_anno:
+            same_label_and_span = None
+            # check if there are multiple labels for the same span
+            if len(span_anno) != len(list(set(s["span"] for s in span_anno))):
+                print("pause")
+                # find the duplicate span: create an ordered dict with the span as key and the number of occurences as value
+                from collections import OrderedDict
+                span_dict = OrderedDict()
+                for s in span_anno:
+                    span_dict[s["span"]] = span_dict.get(s["span"], 0) + 1
+                # find the span that occurs more than once
+                duplicate_spans = []
+                for k, v in span_dict.items():
+                    if v > 1:
+                        duplicate_span = k
+                        duplicate_spans.append(duplicate_span)
+                    else:
+                        continue
+                for d in duplicate_spans:
+                    # check if the span is annotated with the same label:
+                    labels_for_span = [item for item in span_anno if item["span"] == d]
+                    # check if the there are duplicates in the labels_for_span:
+                    if len(labels_for_span)>len(list(set([l["annotation"] for l in labels_for_span]))):
+                        same_label_and_span = (True, labels_for_span)
+                    else:
+                        same_label_and_span = None
+
+            if same_label_and_span is not None:  # if there are any spans which have the same span and label, we want to be careful when searching for them in the string
+                problematic_labels = [l['annotation'] for l in same_label_and_span[1]]
+            else:
+                problematic_labels = []
+            for span in span_anno:
+                span_name = span["annotation"]
+                span_string = span["span"]
+                span_string = self.remove_html_tags(span_string)  # removing any rouge html tags
+                span_onset = None
+                if span_name in problematic_labels:
+                    # check if this is the first mention of this span:
+                    label_names = [s[0] for s in spans]
+                    strings = [s[2] for s in spans]
+                    clean_string = self.remove_html_tags(span['span'])
+                    if clean_string not in strings:
+                        if span_name not in label_names:
+                            pass
+                    else:
+                        span_onset = self.handle_problematic_labels(span_name, span, span_anno, spans)
+                if span_onset is None:
+                    span_onset = self.find_span_onset(span_string) # find the onset of the span in the instance text
+                span_start_end = (span_onset, span_onset+len(span_string))
+                spans.append((span_name, span_start_end, span_string))
+
+
+        return labels, spans
+
+    def add_labeled_instance(self, annotator_id, instance_text, label_annotations, span_annotations, meta_data):
+        self.annotator_id = annotator_id
+        self.instance_text = instance_text
+        self.meta_data = meta_data
+        labels, spans = self.parse_annotations(label_annotations,span_annotations)
+        self.labels = labels
+        self.spans = spans
+
+    def find_span_onset(self, span_string):
+
+        span_onset = self.instance_text.find(span_string)
+        if span_onset == -1: # find returns -1 if the string is not found
+            span_string = span_string.replace('&nbsp;', '')
+            span_onset = self.instance_text.find(span_string)
+        return span_onset
+
+    def handle_problematic_labels(self, span_name, span, span_anno, spans):
+        # if this is the second or third .. mention of this particular label and span, we search in the remainder of the instance text
+        # get the offset of the previous mention of this label and span
+        previous_mention = [s for s in spans if s[2] == self.remove_html_tags(span['span']) and s[2] == self.remove_html_tags(span['span'])]
+        # search for the span string, but only after the off set of the previous mention
+        span_onset = self.instance_text.find(span['span'], previous_mention[0][1][1])
+
+        return span_onset
+
+    def remove_html_tags(self, span_string):
+
+        # something goes wrong with the on and offsets, so we map the string back to the original text and get the offsets this way
+        # check if the annotated span contains an html span container:
+        # using regex to find the span container
+        container = "<span class=[^>]*>"
+        #if container in span_string, replace it with an empty string
+        import re
+        if re.search(container, span_string):
+            span_string = re.sub(container, "", span_string)
+
+        return span_string
+
+
+def instance_stats(list_of_annotators):
+    i = {}
+
+    for annot in list_of_annotators:
+        for inst in annot.instances:
+            if inst.id not in i.keys():
+                i[inst.id] = 1
+            else:
+                i[inst.id] += 1
+    print(i)
+    print([id for id in i.keys()])
+    print(len([id for id in i.keys()]))
+
+def group_instances_wrt_id(list_of_annotators):
+    grouped = {}
+
+    for annot in list_of_annotators:
+        for inst in annot.instances:
+            if inst.id not in grouped.keys():
+                grouped[inst.id] = []
+
+            grouped[inst.id].append(inst)
+
+    return grouped
+
