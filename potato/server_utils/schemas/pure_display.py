4126424dc677e4a344deaa23b9697c764eecbd01
AmelieW <amelie.wuehrl@gmx.de>
2023-06-20 16:02:04 +0200

    adding updated display template

--- potato/server_utils/schemas/pure_display.py
+++ potato/server_utils/schemas/pure_display.py
@@ -4,9 +4,13 @@ Pure Display Layout
 
 
 def generate_pure_display_layout(annotation_scheme):
-    schematic = "<Strong>%s</Strong> %s" % (
+
+    schematic = f"{annotation_scheme['description']} {'<br>'.join(annotation_scheme['labels'])}"
+    schematic = f'<span style="font-size:15pt;">{schematic}</span>'
+
+    schematic_old = "<Strong>%s</Strong> %s" % (
         annotation_scheme["description"],
         "<br>".join(annotation_scheme["labels"]),
     )
-
+    #assert schematic == schematic_old
     return schematic, None
